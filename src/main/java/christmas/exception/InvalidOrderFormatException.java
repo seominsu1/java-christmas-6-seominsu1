36857c5e94aafa679d003402488ad52dca6b50bc
usnim <sms897@naver.com>
2023-11-14 02:23:18 +0900

    [refactor] 에러 문구 변경

--- src/main/java/christmas/exception/InvalidOrderFormatException.java
+++ src/main/java/christmas/exception/InvalidOrderFormatException.java
@@ -1,7 +1,7 @@
 package christmas.exception;
 
 public class InvalidOrderFormatException extends IllegalArgumentException{
-    private static final String INVALID_ORDER_FORMAT_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해주세요.";
+    private static final String INVALID_ORDER_FORMAT_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
 
     public InvalidOrderFormatException() {
         super(INVALID_ORDER_FORMAT_MESSAGE);
--- src/main/java/christmas/exception/InvalidVisitingDayFormatException.java
+++ src/main/java/christmas/exception/InvalidVisitingDayFormatException.java
@@ -1,7 +1,7 @@
 package christmas.exception;
 
 public class InvalidVisitingDayFormatException extends IllegalArgumentException{
-    private static final String INVALID_VISITING_DAY_FORMAT_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해주세요.";
+    private static final String INVALID_VISITING_DAY_FORMAT_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
 
     public InvalidVisitingDayFormatException() {
         super(INVALID_VISITING_DAY_FORMAT_MESSAGE);
