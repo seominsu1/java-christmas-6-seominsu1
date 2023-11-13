2cccdb2b6fc2c8cc1eb677a89eadbb3a1e8a2caf
usnim <sms897@naver.com>
2023-11-14 00:49:03 +0900

    [feat] 출력 기능
    ->  1. 주문 메뉴 출력 기능
        2. 할인 전 총주문 금액 출력 기능
        3. 증정 메뉴 출력 기능
        4. 혜택 내역 출력 기능
        5. 총혜택 금액 출력 기능
        6. 할인 후 예상 결제 금액 출력 기능
        7. 12월 이벤트 배지 출력 기능

---
+++ src/main/java/christmas/MessageBuilder.java
@@ -0,0 +1,116 @@
+package christmas;
+
+import java.text.DecimalFormat;
+import java.util.Map;
+
+public class MessageBuilder {
+
+    private static final String MONTH = "12월";
+    private static final String DATE = "일";
+    private static final String ORDER_MENU_MESSAGE = "<주문 메뉴>";
+    private static final String TOTAL_AMOUNT_MESSAGE = "<할인 전 총주문 금액>";
+    private static final String PRESENTATION_EVENT_MESSAGE = "<증정 메뉴>";
+    private static final String BENEFITS_MESSAGE = "<혜택 내역>";
+    private static final String DISCOUNT_AMOUNT_MESSAGE = "<총혜택 금액>";
+    private static final String PAY_AMOUNT_MESSAGE = "<할인 후 예상 결제 금액>";
+    private static final String BADGE_MESSAGE = "<12월 이벤트 배지>";
+    private static final String AMOUNT = "개";
+    private static final String MONEY = "원";
+    private static final String CHAMPAGNE = "샴페인 1개";
+    private static final String EMPTY = "없음";
+    private static final String[] BENEFITS = {"크리스마스 디데이 할인: ", "평일 할인: ", "주말 할인: ", "특별 할인: ", "증정 이벤트: "};
+
+    private DecimalFormat formatter;
+
+    public MessageBuilder() {
+        this.formatter = new DecimalFormat("###,###");
+    }
+
+    public String buildDateMessage(int date) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(MONTH).append(" ").append(date).append(DATE);
+        return stringBuilder.toString();
+    }
+
+    public String buildOrderMenuMessage(Map<String, Integer> orders) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("\n");
+        stringBuilder.append(ORDER_MENU_MESSAGE);
+        orders.forEach((key, value) -> stringBuilder.append("\n")
+                .append(key)
+                .append(" ")
+                .append(value)
+                .append(AMOUNT));
+        return stringBuilder.toString();
+    }
+
+    public String buildTotalAmountMessage(int amount) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("\n")
+                .append(TOTAL_AMOUNT_MESSAGE)
+                .append("\n")
+                .append(buildByMoneyFormat(amount))
+                .append(MONEY);
+        return stringBuilder.toString();
+    }
+
+    private String buildByMoneyFormat(int money) {
+        return formatter.format(money);
+    }
+
+    public String buildPresentationEventMessage(boolean isPresentationDiscount) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("\n").append(PRESENTATION_EVENT_MESSAGE);
+        if (isPresentationDiscount) {
+            stringBuilder.append("\n")
+                    .append(CHAMPAGNE);
+            return stringBuilder.toString();
+        }
+        stringBuilder.append("\n")
+                .append(EMPTY);
+        return stringBuilder.toString();
+    }
+
+    public String buildBenefitsMessage(int[] benefits) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("\n").append(BENEFITS_MESSAGE);
+        for (int index = 0; index < benefits.length; index++) {
+            if (benefits[index] > 0) {
+                stringBuilder.append("\n")
+                        .append(BENEFITS[index])
+                        .append("-")
+                        .append(formatter.format(benefits[index]))
+                        .append(MONEY);
+            }
+        }
+        return stringBuilder.toString();
+    }
+    public String buildDiscountAmountMessage(int discountAmount) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("\n")
+                .append(DISCOUNT_AMOUNT_MESSAGE)
+                .append("\n")
+                .append("-")
+                .append(formatter.format(discountAmount))
+                .append(MONEY);
+        return stringBuilder.toString();
+    }
+
+    public String buildPayAmountMessage(int payAmount) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("\n")
+                .append(PAY_AMOUNT_MESSAGE)
+                .append("\n")
+                .append(formatter.format(payAmount))
+                .append(MONEY);
+        return stringBuilder.toString();
+    }
+    public String buildBadgeMessage(String badge) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("\n")
+                .append(BADGE_MESSAGE)
+                .append("\n")
+                .append(badge);
+        return stringBuilder.toString();
+    }
+}
---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,53 @@
+package christmas.view;
+
+import christmas.MessageBuilder;
+
+import java.util.Map;
+
+public class OutputView {
+    private static final String OUTPUT_START_MESSAGE = "에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+    private MessageBuilder stringBuilder;
+
+    public OutputView() {
+        this.stringBuilder = new MessageBuilder();
+    }
+    public void readStartMessage(int day) {
+        String dateMessage = stringBuilder.buildDateMessage(day);
+        System.out.println(dateMessage+OUTPUT_START_MESSAGE);
+    }
+
+    public void readOrderMenu(Map<String,Integer> order) {
+        String orderMenuMessage = stringBuilder.buildOrderMenuMessage(order);
+        System.out.println(orderMenuMessage);
+    }
+
+    public void readTotalAmount(int amount) {
+        String totalAmountMessage = stringBuilder.buildTotalAmountMessage(amount);
+        System.out.println(totalAmountMessage);
+    }
+
+    public void readPresentationEvent(boolean isPresentationDiscount) {
+        String presentationEventMessage = stringBuilder.buildPresentationEventMessage(isPresentationDiscount);
+        System.out.println(presentationEventMessage);
+    }
+
+    public void readBenefits(int[] benefits) {
+        String benefitsMessage = stringBuilder.buildBenefitsMessage(benefits);
+        System.out.println(benefitsMessage);
+    }
+
+    public void readDiscountAmount(int discountAmount) {
+        String discountAmountMessage = stringBuilder.buildDiscountAmountMessage(discountAmount);
+        System.out.println(discountAmountMessage);
+    }
+
+    public void readPayAmount(int payAmount) {
+        String payAmountMessage = stringBuilder.buildPayAmountMessage(payAmount);
+        System.out.println(payAmountMessage);
+    }
+
+    public void readBadge(String badge) {
+        String badgeMessage = stringBuilder.buildBadgeMessage(badge);
+        System.out.println(badgeMessage);
+    }
+}
